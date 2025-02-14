dc79dd7bfd832e865c68a1474e526f8041d7b3e8
Eduardos13 <edik.kozelsky@gmail.com>
2025-02-13 22:38:03 -0500

    get css ready
    created all css rules for different color modes, for media

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import 'modern-normalize';
+import '../src/css/styles.css';
 
 createRoot(document.getElementById('root')).render(<App />);
