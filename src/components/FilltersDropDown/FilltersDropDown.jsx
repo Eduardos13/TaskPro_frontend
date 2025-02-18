6513dbb628e086ed7a0b170eed040b0f0fd196a1
Eduardos13 <edik.kozelsky@gmail.com>
2025-02-18 18:47:07 -0500

    screenpage
    for now done - need corrections in future

---
+++ src/components/FilltersDropDown/FilltersDropDown.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import s from './FilltersDropDown.module.css';
+import sprite from '../../icons/all-icons.svg';
+
+const FilltersDropDown = () => {
+  return (
+    <>
+      <div className={s.fillters}>
+        <svg className={s.filltersIcon} width="16" height="16">
+          <use href={`${sprite}#icon-fillters`} />
+        </svg>
+        <span>Fillters</span>
+      </div>
+    </>
+  );
+};
+
+export default FilltersDropDown;
--- src/components/ScreenPage/ScreenPage.jsx
+++ src/components/ScreenPage/ScreenPage.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import s from './ScreenPage.module.css';
 import sprite from '../../icons/all-icons.svg';
+import FilltersDropDown from '../FilltersDropDown/FilltersDropDown';
 
 const ScreenPage = () => {
   return (
     <section className={s.screenPage}>
       <div className={s.container}>
-        <div className={s.fillters}>
-          <svg className={s.filltersIcon} width="16" height="16">
-            <use href={`${sprite}#icon-fillters`} />
-          </svg>
-          <span>Fillters</span>
-        </div>
+        <FilltersDropDown />
         <p className={s.welcomeText}>
           Before starting your project, it is essential{' '}
           <span className={s.createBoard}>to create a board</span> to visualize
