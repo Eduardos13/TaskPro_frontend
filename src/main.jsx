7d9d3093d222ffa42124c5248cd59d71280942b1
Eduardos13 <edik.kozelsky@gmail.com>
2025-08-02 18:22:57 -0400

    create redux and configure auth files

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,15 @@ import '../src/css/reset.css';
 import { ThemeProvider } from './context/ThemeContext.jsx';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </BrowserRouter>
 );
