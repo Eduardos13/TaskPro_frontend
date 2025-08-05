a5b77b0dee7edf2a0b044c50b3660981127ff01d
Eduardos13 <edik.kozelsky@gmail.com>
2025-08-04 20:23:36 -0400

    12

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import s from './Modal.module.css';
 import sprite from '../../icons/all-icons.svg';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectIsModalOpen } from '../../redux/modal/selectors';
+import { closeModal } from '../../redux/modal/slice';
 
 const Modal = ({ children }) => {
+  const dispatch = useDispatch();
+  const isOpen = useSelector(selectIsModalOpen);
+
+  if (!isOpen) {
+    return null;
+  }
+
   return (
-    <div className={s.modalOverlay}>
-      <div className={s.modalContent}>
-        <button className={s.closeBtn}>
+    <div className={s.modalOverlay} onClick={() => dispatch(closeModal())}>
+      <div className={s.modalContent} onClick={(e) => e.stopPropagation()}>
+        <button className={s.closeBtn} onClick={() => dispatch(closeModal())}>
           <svg className={s.closeCross} width="18" height="18">
             <use href={`${sprite}#icon-x-close`} />
           </svg>
